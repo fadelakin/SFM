9e45f10006ca38517ed93838c0232bccadbccdb7
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-06 21:31:37 -0500

    get data

--- app/src/main/java/com/fisheradelakin/interactivestory/StoryActivity.java
+++ app/src/main/java/com/fisheradelakin/interactivestory/StoryActivity.java
@@ -1,39 +1,29 @@
 package com.fisheradelakin.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class StoryActivity extends ActionBarActivity {
 
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
-    }
 
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_story, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(name == null) {
+            name="Friend";
         }
 
-        return super.onOptionsItemSelected(item);
+        Log.d(TAG, name);
     }
 }
