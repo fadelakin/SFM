b5cafab554e8e5520b21b2f863f9f6629d708035
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-08 03:09:48 -0500

    format strings

--- app/src/main/java/com/fisheradelakin/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/fisheradelakin/interactivestory/ui/StoryActivity.java
@@ -23,6 +23,7 @@ public class StoryActivity extends ActionBarActivity {
     private TextView mTextView;
     private Button mChoice1;
     private Button mChoice2;
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +31,20 @@ public class StoryActivity extends ActionBarActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if(name == null) {
-            name="Friend";
+        if(mName == null) {
+            mName="Friend";
         }
 
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
 
         mImageView = (ImageView) findViewById(R.id.storyImageView);
         mTextView = (TextView) findViewById(R.id.storyTextView);
         mChoice1 = (Button) findViewById(R.id.choiceButton1);
         mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage();
     }
 
     private void loadPage() {
@@ -50,7 +53,11 @@ public class StoryActivity extends ActionBarActivity {
         Drawable drawable = getResources().getDrawable(page.getImageId());
         mImageView.setImageDrawable(drawable);
 
-        mTextView.setText(page.getText());
+        String pageText = page.getText();
+        // Add the name if placeholder is included. will not add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
         mChoice1.setText(page.getChoice1().getText());
         mChoice2.setText(page.getChoice2().getText());
     }
